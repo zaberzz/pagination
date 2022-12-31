bd1e879f14a53d9d45842fb6de0b74e1f820d766
Saba <saba.sadegh67@gmail.com>
2022-12-31 12:47:03 +0330

    initial setup,html and CSS

---
+++ app.js
@@ -0,0 +1,13 @@
+import fetchFollowers from "./fetchFollowers.js";
+import displayFollowers from "./displayFollowers.js";
+import paginate from "./paginate.js";
+import displayButtons from "./displayButtons.js";
+
+const title = document.querySelector(".section-title h1");
+const init = async () => {
+  const followers = fetchFollowers();
+  displayFollowers(followers);
+  title.textContent = "Pagination";
+};
+
+window.addEventListener("load", init);
