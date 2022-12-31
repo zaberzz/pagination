9d13989e0d2c16646c6ce89e4c09b42a1a1e606d
Saba <saba.sadegh67@gmail.com>
2022-12-31 19:55:29 +0330

    add functionality to prev and next btns

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ let pages = [];
 
 const setupUI = () => {
   displayFollowers(pages[index]);
+  displayButtons(btnContainer, pages, index);
 };
 const init = async () => {
   const followers = await fetchFollowers();
@@ -20,4 +21,23 @@ const init = async () => {
   setupUI();
 };
 
+btnContainer.addEventListener("click", function (e) {
+  if (e.target.classList.contains("btn-container")) return;
+  if (e.target.classList.contains("page-btn")) {
+    index = parseInt(e.target.dataset.index);
+  }
+  if (e.target.classList.contains("next-btn")) {
+    index++;
+    if (index > pages.length - 1) {
+      index = 0;
+    }
+  }
+  if (e.target.classList.contains("prev-btn")) {
+    index--;
+    if (index < 0) {
+      index = pages.length - 1;
+    }
+  }
+  setupUI();
+});
 window.addEventListener("load", init);
